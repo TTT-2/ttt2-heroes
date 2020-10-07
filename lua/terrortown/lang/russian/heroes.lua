85b17406779ee6d13e66332b47616f2ef98ea922
Blueberryy <igory.ygr200@gmail.com>
2020-10-07 18:28:24 +0500

    update

---
+++ lua/terrortown/lang/russian/heroes.lua
@@ -0,0 +1,41 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SUPERVILLAIN.name] = "Суперзлодей"
+L["info_popup_" .. SUPERVILLAIN.name] = [[Вы суперзлодей! Попробуйте уничтожить несколько кристаллов своим ножом, чтобы заработать себе кредиты!
+Уничтожение кристаллов также удаляет геройские способности владельца.]]
+L["body_found_" .. SUPERVILLAIN.abbr] = "Он был суперзлодеем!"
+L["search_role_" .. SUPERVILLAIN.abbr] = "Этот человек был суперзлодеем!"
+L["target_" .. SUPERVILLAIN.name] = "Суперзлодей"
+L["ttt2_desc_" .. SUPERVILLAIN.name] = [[Суперзлодей - предатель (который работает вместе с другими предателями)! Цель состоит в том, чтобы убить всех остальных ролей, кроме других ролей предателей. Суперзлодей может уничтожать кристаллы своих врагов, чтобы получить кредиты и удалить их способности героя.]]
+
+-- OTHER LANGUAGE STRINGS
+L["credit_h_all"] = "Вы получили {num} кредитов, уничтожив кристалл."
+
+L["ttt2_heroes_crystal_already_placed"] = "Вы уже разместили кристалл!"
+L["ttt2_heroes_not_on_ground"] = "Вы должны стоять на твёрдой земле, чтобы разместить свой кристалл!"
+L["ttt2_heroes_crystal_placed"] = "Ваш кристалл успешно размещён!"
+L["ttt2_heroes_crystal_picked_up"] = "Вы успешно подобрали свой кристалл!"
+L["ttt2_heroes_crystal_destoyed"] = "Ваш кристалл был уничтожен!"
+L["ttt2_heroes_ability_disabled"] = "Вы не можете использовать свою способность, так как не разместили свой кристалл!"
+L["ttt2_heroes_crystal_already_picked_up"] = "Вы можете забрать свой кристалл только {num} раз!"
+L["ttt2_heroes_crystal_already_no_pickup"] = "Вы не можете забрать свой кристалл!"
+L["ttt2_heroes_all_crystals_destroyed"] = "Все кристаллы были уничтожены!"
+L["ttt2_heroes_crystal_ability_pickup_disabled"] = "Вы не можете подобрать свой кристалл, так как у вас нет способности!"
+L["ttt2_heroes_crystal_auto_placed"] = "Попытка автоматически разместить кристалл. Вы можете отключить эту функцию в локальных настройках героев."
+
+L["ttt2_heroes_bind_place"] = "Разместить кристалл"
+L["ttt2_heroes_settings_crystal"] = "Кристалл"
+L["ttt2_heroes_entity_crystal"] = "Кристалл Героев"
+L["ttt2_heroes_entity_crystal_owner_self"] = "Вы владелец этого кристалла!"
+L["ttt2_heroes_entity_crystal_owner_unknown"] = "Владелец этого кристалла неизвестен"
+L["ttt2_heroes_entity_crystal_owner"] = "Владелец кристалла: "
+L["ttt2_heroes_entity_crystal_knife"] = "Используйте свой кристальный нож, чтобы получить кредиты на снаряжение"
+L["ttt2_heroes_entity_crystal_cant_interact"] = "Вы не можете взаимодействовать с этим кристаллом"
+L["ttt2_heroes_entity_crystal_destroy"] = "РАЗРУШИТЬ"
+
+L["submenu_addons_heroes_title"] = "Герои TTT2"
+L["header_addons_heroes"] = "Общие настройки героев"
+L["label_heroes_auto_place_enable"] = "Должна ли система пытаться автоматически поставить кристалл после выбора героя?"
+
+L["header_bindings_heroes"] = "Герои TTT2"
