5b938fa42a5d9737a44d00e259f8d3a6ce00aef8
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 02:03:52 +0100

    Create heroes.lua
    french translation : TTT2 Heroes

---
+++ lua/terrortown/lang/french/heroes.lua
@@ -0,0 +1,41 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SUPERVILLAIN.name] = "Super-Vilain"
+L["info_popup_" .. SUPERVILLAIN.name] = [[Vous êtes un Super-Vilain! Essayez de détruire des cristaux avec votre couteau pour gagner des crédits!
+La destruction des cristaux supprime également le pouvoir des héros a qui ils appartenaient..]]
+L["body_found_" .. SUPERVILLAIN.abbr] = "C'était un Super-Vilain!"
+L["search_role_" .. SUPERVILLAIN.abbr] = "Cette personne était un Super-Vilain!"
+L["target_" .. SUPERVILLAIN.name] = "Super-Vilain"
+L["ttt2_desc_" .. SUPERVILLAIN.name] = [[Le Super-Vilain est un traître (qui travaille avec les autres traîtres)! Son but est de tuer tous les autres rôles, à l'exception des autres  traître. Le Super-Vilain est capable de détruire les cristaux de ses ennemis pour obtenir des crédits et leur retirer leurs pouvoirs de héros.]]
+
+-- OTHER LANGUAGE STRINGS
+L["credit_h_all"] = "Vous avez été récompensé de {num} crédit(s) d'équipement pour avoir détruit un cristal."
+
+L["ttt2_heroes_crystal_already_placed"] = "Vous avez déjà placé un cristal!"
+L["ttt2_heroes_not_on_ground"] = "Vous devez toucher le sol lorsque vous placez votre cristal!"
+L["ttt2_heroes_crystal_placed"] = "Votre cristal a été placé avec succès!"
+L["ttt2_heroes_crystal_picked_up"] = "Vous avez ramassé votre cristal!"
+L["ttt2_heroes_crystal_destoyed"] = "Votre cristal a été détruit!"
+L["ttt2_heroes_ability_disabled"] = "Vous ne pouvez pas utiliser votre pouvoir car vous n'avez pas placé votre cristal!"
+L["ttt2_heroes_crystal_already_picked_up"] = "Vous pouvez récupérer votre cristal uniquement {num} fois!"
+L["ttt2_heroes_crystal_already_no_pickup"] = "Vous ne pouvez pas ramasser votre cristal!"
+L["ttt2_heroes_all_crystals_destroyed"] = "Tous les cristaux ont été détruits!"
+L["ttt2_heroes_crystal_ability_pickup_disabled"] = "Vous ne pouvez pas ramasser votre cristal parce que vous n'avez plus de pouvoir!"
+L["ttt2_heroes_crystal_auto_placed"] = "Tentative de placement automatique de votre cristal. Vous pouvez désactiver cette fonction dans les paramètres généraux du mode TTT2 Heroes."
+
+L["ttt2_heroes_bind_place"] = "Placer le Cristal"
+L["ttt2_heroes_settings_crystal"] = "Cristal"
+L["ttt2_heroes_entity_crystal"] = "Cristal des héros"
+L["ttt2_heroes_entity_crystal_owner_self"] = "Vous êtes le propriétaire de ce cristal!"
+L["ttt2_heroes_entity_crystal_owner_unknown"] = "Le propriétaire de ce cristal est inconnu"
+L["ttt2_heroes_entity_crystal_owner"] = "Propriétaire du cristal: "
+L["ttt2_heroes_entity_crystal_knife"] = "Utilisez votre couteau en cristal pour obtenir des crédits d'équipement"
+L["ttt2_heroes_entity_crystal_cant_interact"] = "Vous ne pouvez pas interagir avec ce cristal"
+L["ttt2_heroes_entity_crystal_destroy"] = "DÉTRUIRE"
+
+L["submenu_addons_heroes_title"] = "TTT2 Heroes"
+L["header_addons_heroes"] = "Paramètres généraux du mode TTT2 Heroes"
+L["label_heroes_auto_place_enable"] = "Le système devrait-il essayer de placer le cristal automatiquement après la sélection des héros?"
+
+L["header_bindings_heroes"] = "TTT2 Heroes"
